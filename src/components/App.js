e3bc277a3cd44b22b274377bb6c0adff149302f5
Jeffrey  Lonergan <jeffreylonergan@Jeffreys-MacBook-Pro.local>
2018-06-03 16:37:17 -0700

    Building out state

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,74 @@
 import React, { Component } from 'react';
-import  Search  from './search';
+import  Search  from './Search';
+import logo from '../../assets/Google-books-logo.png';
 
 export default class App extends Component {
 
-    // searchBooks = () => {
-    //     const{ title } = this.state;
-    // }
+    state = { 
+        topic: '',
+        loading: false,
+        error: null,
+        totalResults: 0,
+        page: 1,
+        perPage: 20,
+        books: [],
+        startIndex:0,
+        searched: false,
+    };
+
+    searchBooks = () => {
+        const { topic, page, perPage } = this.state;
+    
+        this.setState({ loading: true });
+        
+        search({ topic }, { page, perPage })
+          .then(({ books, totalResults }) => {
+            this.setState({ books, totalResults, error: null });
+          }, error => {
+            this.setState({ error });
+          })
+          .then(() => this.setState({ loading: false }));
+    
+      };
+    
+    handleSearch = ({ search }) => {
+        this.setState({ topic: search}, this.searchBooks);
+    };
+
+    handlePage = ({ page }) => {
+        this.setState({ page }, this.searchBooks);
+    }
 
     render() {
 
-        const title = this.state;
+    const { books, loading, totalResults, page, perPage, error } = this.state;
+
 
         return (
             <div>
             <header>
+                <div className="header-container">
+                <h1>Google Book Search</h1>
+                {/* <img src = {logo}/> */}
+                </div>
+                <div clasName="search-container">
                 <Search onSearch={this.handleSearch}/>
+                </div>
             </header>
+            <main>
+                <section className= "main-container">
+                    {loading && <div>Loading ...</div>}
+                    {error && <div>Error : {error.message}</div> }
+                    </section>
+                    {/* <section>
+                        <Paging     
+                        totalResults = {totalResults}
+                        page={page}
+                        perPage={perPage}
+                        onPage={this.handlePage}/>
+                        <Books books={books}/>
+                        </section> */}
+                        </main>
             </div>
         )
     }
