9f0a7e80a756967255ae43b4006b54bb257db2b6
Jeffrey  Lonergan <jeffreylonergan@Jeffreys-MacBook-Pro.local>
2018-06-03 18:47:50 -0700

    fixed BASE_URL link

--- src/services/booksAPI.js
+++ src/services/booksAPI.js
@@ -1,6 +1,6 @@
-const BASE_URL = 'https://www.googleapis.com/books/v1/';
+const BASE_URL = 'https://www.googleapis.com/books/v1';
 const BOOKVOLUMES_URL = `${BASE_URL}/volumes?`;
-const SORT_QUERY = 'sortBy=publishedAt';
+const SORT_QUERY = 'ordyBy=relevance';
 const maxResults = 10;
 
 const throwJson = json => { throw json; };
@@ -12,7 +12,7 @@ export function search({ topic, sources = ['bad'] }, { page = 1, pageSize = 10})
     const search = `&q=${topic}&sources=${sources.join()}`;
     const paging = `&page=${page}&pageSize=${pageSize}`;
     const sort = `&${SORT_QUERY}`;
-    const bookIndex = (page * pageSize) -pageSize;
+    const bookIndex = (page * pageSize) - pageSize;
     const startIndex = `&startIndex=${bookIndex}`;
 
     return get(`${BOOKVOLUMES_URL}${search}${paging}${sort}${startIndex}`);
