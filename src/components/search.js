e59a73d4b87cf2028defc9f92d6c2a214a7e4806
jeffrey lonergan <jrlone1990@gmail.com>
2018-06-05 15:01:17 -0700

    CORS problem might be a hang-up error

--- src/components/search.js
+++ src/components/search.js
@@ -21,17 +21,16 @@ export default class Search extends Component {
       const { search } = this.state;
 
       return (
-        <div className = {styles.search}>
-          <form className = "search-form" onSubmit={event => this.handleSubmit(event)}>
-            <label>
-                    Search by name: &nbsp;
-              <input value={search} onChange={this.handleChange}/>
-            </label>
-            <button className = "search-button">Find</button>
-
-          </form>
-        </div>
+        <form className = {styles.search} onSubmit={event => this.handleSubmit(event)}>
+          <label>
+            Search by name: &nbsp;
+            <input value={search} onChange={this.handleChange}/>
+          </label>
+          <button className="search-button">Find</button>
+        </form>
       );
+    
     }
+  
 
 }
