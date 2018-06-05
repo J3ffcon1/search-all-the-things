8e1eb042271caf32063862ca75f287b2a0b312e2
jeffrey lonergan <jrlone1990@gmail.com>
2018-06-05 00:52:02 -0700

    fixing lint errors

--- src/components/search.js
+++ src/components/search.js
@@ -4,31 +4,31 @@ import styles from '../styles/Search.css';
 export default class Search extends Component {
 
     state = {
-        search: ''
+      search: ''
     };
 
     handleChange = ({ target }) => {
-        this.setState({ search: target.value });
+      this.setState({ search: target.value });
     };
 
     handleSubmit = event => {
-        event.preventDefault();
-        this.props.onSearch(this.state);
+      event.preventDefault();
+      this.props.onSearch(this.state);
     };
 
     render() {
-        const { search } = this.state;
+      const { search } = this.state;
 
-        return (
-            <form className = "search-form" onSubmit={event => this.handleSubmit(event)}>
-                <label>
+      return (
+        <form className = "search-form" onSubmit={event => this.handleSubmit(event)}>
+          <label>
                     Search by name: &nbsp;
-                    <input value={search} onChange={this.handleChange}/>
-                </label>
-                <button className = "search-button">Find</button>
+            <input value={search} onChange={this.handleChange}/>
+          </label>
+          <button className = "search-button">Find</button>
 
-                </form>
-        );
+        </form>
+      );
     }
 
 }
